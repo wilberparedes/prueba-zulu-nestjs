6350f2fa2bf62fb7ae41d4589ba46fa674ea906e
Wilber Paredes <wilberparedes@gmail.com>
2022-08-28 01:51:43 -0500

    feat: import use db mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientModule,
+    MongooseModule.forRoot('mongodb://localhost/prueba-zulu-nestjs'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
