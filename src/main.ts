f0ca0d77f6f062e6d147db893f178ed93085642b
Wilber Paredes <wilberparedes@gmail.com>
2022-08-29 03:02:26 -0500

    feat: add config prod

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .addBearerAuth()
     .setTitle('Swagger')
@@ -12,6 +13,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
